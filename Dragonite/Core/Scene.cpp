c5edf270a2bb2e18b2a99f1f00e15abaa0f6ec54
Spyro (Home) <epicdudeness677@gmail.com>
2022-07-28 00:27:48 +0200

    Some scene changes.

--- Dragonite/Core/Scene.cpp
+++ Dragonite/Core/Scene.cpp
@@ -30,7 +30,16 @@ void Scene::Awake()
 	obj->myTransform.SetPosition({ 0,2, 10 });
 	myObjectsToUpdate.push_back(obj);
 
-	myObjectsToUpdate.push_back(obj);
+
+
+	for (int i = 0; i < 10; i++)
+	{
+		auto& obj2 = myRuntime->CreateGameObject();
+		obj2->myName = "A Cube";
+		obj2->AddComponent<ModelRenderer>()->LoadModel(UNITCUBE);
+		obj2->myTransform.SetPosition({ (float)i,i + 1.0f, i + 10.0f });
+		myObjectsToUpdate.push_back(obj2);
+	}
 
 	auto& objB = myRuntime->CreateGameObject();
 	objB->myName = "A Pyramid";
